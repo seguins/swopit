b38e80615af9e978530e26a747a10398d4996fac
Stéphane Seguin <stephseguin93@gmail.com>
2015-05-11 22:47:27 -0400

    manage page

--- loggedin/handlers.py
+++ loggedin/handlers.py
@@ -7,19 +7,20 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import ndb, db
 import math
 
+NUMBER_ELEMENT_PER_PAGE = 5
+
 class ListHandler(BaseHandler):
-  NUMBER_ELEMENT_PER_PAGE = 10
   def get(self, page_number, category = -1):
     if page_number != None:
       page_number = int(page_number.replace("/", ""))
     else:
       page_number = 0
-    offset = page_number * self.NUMBER_ELEMENT_PER_PAGE;
+    offset = page_number * NUMBER_ELEMENT_PER_PAGE;
     if category == -1:
       query = Ad.query().order(-Ad.created)
     else:
       query = Ad.query(Ad.category == int(category)).order(-Ad.created)
-    ads = query.fetch(self.NUMBER_ELEMENT_PER_PAGE, offset=offset)
+    ads = query.fetch(NUMBER_ELEMENT_PER_PAGE, offset=offset)
     for ad in ads:
       if ad.user != None:
         u = ad.user.get()
@@ -30,11 +31,36 @@ class ListHandler(BaseHandler):
       'ads': ads,
       'current_page': page_number + 1,
       'category': int(category),
-      'pages': range(1, int(math.ceil(float(query.count()) / float(self.NUMBER_ELEMENT_PER_PAGE))) + 1)
+      'pages': range(1, int(math.ceil(float(query.count()) / float(NUMBER_ELEMENT_PER_PAGE))) + 1)
     }
     if category != -1:
       param["categoryStr"] = models.categories[int(category)];
 
+    self.render_template('list.html', param)
+
+
+class ManageHandler(BaseHandler):
+  @user_required
+  def get(self, page_number):
+    if page_number != None:
+      page_number = int(page_number.replace("/", ""))
+    else:
+      page_number = 0
+    offset = page_number * NUMBER_ELEMENT_PER_PAGE;
+    query = Ad.query(Ad.user == self.user.key).order(-Ad.created)
+    ads = query.fetch(NUMBER_ELEMENT_PER_PAGE, offset=offset)
+    for ad in ads:
+      if ad.user != None:
+        u = ad.user.get()
+        ad.email = u.email_address
+        ad.phone = u.phone
+        ad.displayPhone = u.displayPhone
+    param = {
+      'ads': ads,
+      'current_page': page_number + 1,
+      'manage': True,
+      'pages': range(1, int(math.ceil(float(query.count()) / float(NUMBER_ELEMENT_PER_PAGE))) + 1)
+    }
     self.render_template('list.html', param)
 
 class LogoutHandler(BaseHandler):
@@ -62,29 +88,11 @@ class CreateHandler(BaseHandler):
     image = images.resize(image, 250, 320)
     
     # Create and save into datastore
-    print category
-    print models.categories
-    print models.categories.index(category)
     ad = Ad(title=title, info=info, image=image, user=self.user.key, category=categories.index(category))
     ad.put()
     
     # Redirect to the product page
-    self.redirect('/view/' + ad.key.urlsafe())
-
-class ViewHandler(BaseHandler):
-  @user_required
-  def get(self, urlsafe):
-    key = ndb.Key(urlsafe=urlsafe)
-    ad = key.get()
-
-    if ad == None:
-      self.notFound()
-
-    self.render_template('view.html', {
-      'info': ad.info,
-      'title': ad.title,
-      'safe_image_url': '/photo/' + urlsafe
-      })
+    self.redirect('/manage')
 
 
 class ImageHandler(webapp2.RequestHandler):
@@ -98,11 +106,16 @@ class ImageHandler(webapp2.RequestHandler):
     else:
         self.response.out.write('No image')
 
-class ManageHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect('/')
-
 class FilterHandler(BaseHandler):
   @user_required
   def post(self):
     self.redirect('/list/0/' + str(models.categories.index(self.request.get('category'))))
+
+class DeleteHandler(BaseHandler):
+  @user_required
+  def get(self, urlsafe):
+    key = ndb.Key(urlsafe=urlsafe)
+    ad = key.get()
+    if ad.user == self.user.key:
+      key.delete()
+    self.redirect('/manage/')
